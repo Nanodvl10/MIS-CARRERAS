366a9e4702adc5ed65f7322fe34212a5eac1607c
Nanodvl10 <rubendvl10@gmail.com>
2026-09-09 17:47:39 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'carreras-v1';
+const CACHE = 'carreras-v2';
 const ASSETS = [
   './','index.html','styles.css','app.js','profile.js',
   'races/corral-del-diablo.js','manifest.json',
